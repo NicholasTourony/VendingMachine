d97b19a995509d7ee4d45dcc5098e83cfcb9e411
NicholasTourony <natourony@quinnipiac.edu>
2024-02-19 15:07:25 -0500

    Added prompt for snacks and dollar values

--- VendingMachine.java
+++ VendingMachine.java
@@ -1,5 +1,5 @@
 public class VendingMachine {
     public static void main(String[] argx){
-        
+        System.out.println("What would you like to order enter 1 for a chocolate bar ($1.50), 2 for a soda ($1.00), or 3 for chips ($0.75):");
     }
 }
